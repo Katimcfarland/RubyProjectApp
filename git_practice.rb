cf186bbe9ac26010f0e7617979f935fe0c43ccf4
“Katherine <katimcfarland1@gmail.com>
2019-10-07 18:38:03 -0400

    adding all my ruby files to this repo

---
+++ git_practice.rb
@@ -0,0 +1,65 @@
+<html>
+
+<head>
+
+<title>Kati Recipe</title>
+<head/>
+
+<body>
+<h1>Hot and Sour soup</h1>
+ <ul>
+        <li><a href="#description">Description</a></li>
+        <li><a href="#ingredients">Ingredients</a></li>
+        <li><a href="#directions">Directions</a></li>
+      </ul>
+<ul>
+    <li>Broth</li>
+    <li>Mushrooms</li>
+    <li>Scallions</li>
+    <li>Soy Sauce</li>
+</ul>
+<p>This is the best soup ever, jeff loves it and I love making it.</p>
+
+
+<img src="appacademy.io/images/app-academy-logo.img" />
+<a href="http://appacademy.io">App Academy</a>
+<a href="#top-of-page">Back to top</a> 
+<img src="http://appacademy.io/images/app-academy-logo.img" alt="app-academy-logo" />
+
+<label for="rating">Rating</label>
+      1<input type="radio" name="rating" value="1" checked>
+      2<input type="radio" name="rating" value="2">
+      3<input type="radio" name="rating" value="3">
+      4<input type="radio" name="rating" value="4">
+      5<input type="radio" name="rating" value="5">
+
+         <label for="expertise">Cooking Expertise</label>
+
+      <select id="expertise" name="expertise">
+        <option value="Select Cooking Expertise" disabled="true" selected="true">Select Cooking Experise</option>
+
+        <option value="Sometimes Cook">Sometimes Cook</option>
+
+        <option value="Cook Everyday">Cook Everyday</option>
+
+        <option value="Cooking is a Hobby">Cooking is a Hobby</option>
+
+        <option value="Professional Chef">Professional Chef</option>
+      </select>
+
+      <label id="reviews">Review
+        <textarea name="review" cols="30" rows="10"></textarea>
+      </label>
+
+      <input type="submit" value="Submit Review">
+    </form>
+  </section>
+
+      <label for="expertise">Cooking Expertise</label>
+
+
+<input type="button" value="Click Me"
+
+<form action
+<body/>
+</html>
---
+++ word_chains.rb
@@ -0,0 +1,33 @@
+def initialize(dictionary_file_name)
+    @dictionary = Set.new(words)
+
+    def adjacent_words(word)
+        words = []
+        new_current_words = ""
+
+        word.each_with_index do |i|
+            word[i] + word[i + 1]
+        end 
+    if i !word.length - 1
+        words << word[i] + word[i + 1]
+    end 
+    words
+end 
+
+def run(source, target)
+    source = @current_words
+    source = @all_seen_words
+
+    arr.each do |ele|
+        ele.current_words
+    self.current_words unless current_words.empty? 
+    
+    arr.each do |word|
+        ele.new_current_words do |word|
+            if word !@all_seen_words 
+                skip
+            else word << new_current_words
+
+                
+
+
